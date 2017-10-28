e170dbd56f0534ed8ad1433fb202ba93576383c0
Benjamin Lasseur <benjamin.lasseur@gmail.com>
2017-10-28 19:46:23 +0200

    Ajout code erreur lors de la création d'un collaborateur

--- src/main/java/dev/sgp/web/NouveauCollaborateurController.java
+++ src/main/java/dev/sgp/web/NouveauCollaborateurController.java
@@ -2,6 +2,8 @@ package dev.sgp.web;
 
 import java.io.IOException;
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.stream.Collectors;
 
 import javax.servlet.ServletException;
@@ -27,11 +29,26 @@ public class NouveauCollaborateurController extends HttpServlet {
 	 */
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		String nom = req.getParameter("nom");
-		String prenom = req.getParameter("prenom");
-		LocalDate date = LocalDate.parse(req.getParameter("anniversaire"));
-		String adresse = req.getParameter("adresse");
-		String numSecu = req.getParameter("numsecu");
+		List<String> parametres = new ArrayList<>();
+		parametres.add("nom");
+		parametres.add("prenom");
+		parametres.add("anniversaire");
+		parametres.add("adresse");
+		parametres.add("numsecu");
+		String nom = req.getParameter(parametres.get(0));
+		String prenom = req.getParameter(parametres.get(1));
+		String dateString = req.getParameter(parametres.get(2));
+		LocalDate date = LocalDate.parse(dateString);
+		String adresse = req.getParameter(parametres.get(3));
+		String numSecu = req.getParameter(parametres.get(4));
+		if(parametres.stream().filter(p->p.equals("")).findAny().isPresent()) {
+			StringBuilder str = new StringBuilder("Le(s) paramètre(s) suivant(s) manque(nt): ");
+			parametres.stream().forEach(p -> {if(p.equals("")) {
+			str.append(" ").append(p);	
+			}
+			});
+			resp.sendError(400, str.toString());
+		}
 		Collaborateur c = new Collaborateur(nom, prenom, date, adresse, numSecu);
 		Constantes.COLLAB_SERVICE.sauvegarderCollaborateur(c);
 		req.setAttribute("collaborateurs", Constantes.COLLAB_SERVICE.listerCollaborateurs().stream()
